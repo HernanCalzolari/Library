f668bdd8c775514fbbedc78d946cef25b6b2c414
Hernan Calzolari <pigote@gmail.com>
2023-11-21 02:37:13 -0300

    Scripts file created

---
+++ scripts.js
@@ -0,0 +1,65 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  displayBooks();
+}
+
+function displayBooks() {
+  const libraryContainer = document.getElementById("library-container");
+  libraryContainer.innerHTML = ""; // Clear previous content
+
+  myLibrary.forEach((book, index) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Status: ${book.read ? "Read" : "Not Read"}</p>
+      <button onclick="removeBook(${index})">Remove</button>
+      <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>
+    `;
+    libraryContainer.appendChild(bookCard);
+  });
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+function toggleReadStatus(index) {
+  myLibrary[index].read = !myLibrary[index].read;
+  displayBooks();
+}
+
+document.getElementById("new-book-btn").addEventListener("click", () => {
+  const newBookForm = document.getElementById("new-book-form");
+  newBookForm.classList.toggle("hidden");
+});
+
+document.getElementById("book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").checked;
+
+  const newBook = new Book(title, author, pages, read);
+  addBookToLibrary(newBook);
+
+  // Reset form
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("read").checked = false;
+});
